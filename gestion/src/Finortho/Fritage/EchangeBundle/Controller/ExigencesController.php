078c59389f3d846072eb2d346bbf300393ce1936
KristenGarnier <garnier.kristen@icloud.com>
2015-10-28 12:09:58 +0100

    Automatisation du file tree

--- gestion/src/Finortho/Fritage/EchangeBundle/Controller/ExigencesController.php
+++ gestion/src/Finortho/Fritage/EchangeBundle/Controller/ExigencesController.php
@@ -31,6 +31,8 @@ class ExigencesController extends Controller
     }
 
     public function testAction(){
-        return $this->render('FinorthoFritageEchangeBundle:Test:index.html.twig');
+        $file_tree = $this->get('finortho_fritage_echange.file_tree');
+        $tree = $file_tree->php_file_tree($this->get('kernel')->getRootDir() . '/../web/uploads/commandes_utilisateurs', "/uploads/commandes_utilisateurs/[link]" );
+        return $this->render('FinorthoFritageEchangeBundle:Test:index.html.twig', array('tree' => $tree));
     }
 }
