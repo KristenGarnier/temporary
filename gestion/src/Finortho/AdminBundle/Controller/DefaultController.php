d924db690fa49b91812127e4b149acf4a68f7289
KristenGarnier <garnier.kristen@icloud.com>
2015-10-29 09:16:47 +0100

    Mise en place des utilisateurs dans l'admin

--- gestion/src/Finortho/AdminBundle/Controller/DefaultController.php
+++ gestion/src/Finortho/AdminBundle/Controller/DefaultController.php
@@ -2,12 +2,50 @@
 
 namespace Finortho\AdminBundle\Controller;
 
+use Finortho\Fritage\EchangeBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('FinorthoAdminBundle:Default:index.html.twig', array('name' => $name));
+        $file_tree = $this->get('finortho_fritage_echange.file_tree');
+        $tree = $file_tree->php_file_tree($this->get('kernel')->getRootDir() . '/../web/uploads/commandes_utilisateurs', "/uploads/commandes_utilisateurs/[link]");
+        return $this->render('FinorthoAdminBundle:Default:index.html.twig', array('tree' => $tree));
+    }
+
+    public function addUserAction(Request $request)
+    {
+        $userManager = $this->get('fos_user.user_manager');
+        $user = $userManager->createUser();
+        if ($this->get('request')->getMethod() == 'POST') {
+
+            $user->setUsername($request->get('username'));
+            $user->setEmail($request->get('email'));
+            $user->setPlainPassword($request->get('password'));
+            $user->setEnabled(true);
+
+            $exists = $userManager->findUserBy(array('email' => $user->getEmail()));
+            if ($exists instanceof User) {
+                throw new \HttpException(409, 'Email already taken');
+            }
+
+            $existsUsername = $userManager->findUserBy(array('username' => $user->getUsername()));
+            if ($existsUsername instanceof User) {
+                throw new \HttpException(409, 'Username already taken');
+            }
+
+            $userManager->updateUser($user);
+        }
+
+        return $this->render('FinorthoAdminBundle:Default:newuser.html.twig');
+    }
+
+    public function listUserAction(){
+        $userManager = $this->get('fos_user.user_manager');
+        $users = $userManager->findUsers();
+
+        return $this->render('FinorthoAdminBundle:Default:users.html.twig', array('users' => $users));
     }
 }
--- gestion/web/js/button_effect.js
+++ gestion/web/js/button_effect.js
@@ -1,13 +1,15 @@
-document.querySelector("#actions").addEventListener("click", function (e) {
+    document.querySelector("#actions").addEventListener("click", event);
+
+
+function test(that) {
+    that.classList.toggle('anim');
+}
+
+function event(e) {
     if (e.target !== e.currentTarget) {
         test(e.target);
         setTimeout(function () {
             test(e.target);
         }, 1000);
     }
-
-});
-
-function test(that) {
-    that.classList.toggle('anim');
 }
