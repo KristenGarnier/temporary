ac91912d005a42ca04f9fbb9073fb153cbe470fe
KristenGarnier <garnier.kristen@icloud.com>
2015-12-16 14:59:28 +0100

    Mise en place fonctionnelle de l'api

---
+++ gestion/src/Finortho/ApiBundle/Controller/ProduitController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Finortho\ApiBundle\Controller;
+
+use FOS\RestBundle\Controller\FOSRestController;
+use Symfony\Component\HttpFoundation\Request;
+
+class ProduitController extends FOSRestController
+{
+    public function getProduitAction(Request $request, $id)
+    {
+        return 'For fuck sake, just work plz';
+    }
+
+    public function getProduitsAction(Request $request)
+    {
+        return 'For fuck sake, just work plz';
+    }
+
+    public function postProduitAction(Request $request)
+    {
+        return 'For fuck sake, just work plz';
+    }
+}
